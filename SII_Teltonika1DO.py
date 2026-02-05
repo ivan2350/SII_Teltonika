1f597c0ac28e28d9d11f50893a3012ac3ca03bcb
ivan2350 <gonzalezivan0025@gmail.com>
2026-02-05 10:48:55 -0600

    Update SII_Teltonika1DO.py

--- SII_Teltonika1DO.py
+++ SII_Teltonika1DO.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import time
-from pymodbus.client.sync import ModbusSerialClient
+from pymodbus.client import ModbusSerialClient
 
 # ================= CONFIGURACIÓN =================
 
