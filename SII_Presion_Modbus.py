ff055159efa11fcbd0dc7ccce8ce5744080b2277
ivan2350 <gonzalezivan0025@gmail.com>
2026-02-12 09:24:32 -0600

    Update SII_Presion_Modbus.py

--- SII_Presion_Modbus.py
+++ SII_Presion_Modbus.py
@@ -1,12 +1,14 @@
-#!/usr/bin/env python3
-import time
-import struct
 from pymodbus.client import ModbusTcpClient
+import struct
 
 client = ModbusTcpClient("127.0.0.1", port=502)
-
 client.connect()
 
-result = client.read_holding_registers(0, 2, unit=1)
+result = client.read_holding_registers(address=0, count=2, unit=0)
 
 print(result)
+
+if not result.isError():
+    raw = struct.pack('>HH', result.registers[1], result.registers[0])
+    presion = struct.unpack('>f', raw)[0]
+    print("Presión:", presion)
