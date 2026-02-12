e41d3e2c9bdb385d583693af7ff7a05c16a5d5d3
ivan2350 <gonzalezivan0025@gmail.com>
2026-02-12 09:06:29 -0600

    Update SII_Presion_Modbus.py

--- SII_Presion_Modbus.py
+++ SII_Presion_Modbus.py
@@ -3,30 +3,10 @@ import time
 import struct
 from pymodbus.client import ModbusTcpClient
 
-def ts():
-    return time.strftime("%d-%m-%Y %H:%M:%S")
-
 client = ModbusTcpClient("127.0.0.1", port=502)
 
-if not client.connect():
-    print("❌ No se pudo conectar a Modbus TCP")
-    exit()
+client.connect()
 
-print("✅ Conectado a Modbus TCP local")
+result = client.read_holding_registers(0, 2, unit=1)
 
-while True:
-    try:
-        result = client.read_holding_registers(address=1, count=2, unit=1)
-
-        if result.isError():
-            print("Error lectura")
-        else:
-            raw = struct.pack('>HH', result.registers[1], result.registers[0])
-            presion = struct.unpack('>f', raw)[0]
-            print(f"[{ts()}] Presión: {presion:.2f} PSI")
-
-        time.sleep(5)
-
-    except Exception as e:
-        print("ERROR:", e)
-        time.sleep(3)
+print(result)
